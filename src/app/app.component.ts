86c233c708d5826be9a4570b903aff2e01eadf4e
jorgehenriquezd <57832978+jorgehenriquezd@users.noreply.github.com>
2022-04-18 10:06:17 -0400

    home-init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'mudy-client';
 
-   view = 'partners';
+   view = 'home';
 
   ngOnInit(): void {
    
