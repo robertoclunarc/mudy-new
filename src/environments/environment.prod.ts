f2723660d791e83f598ef0f8a942333dfa99f5e9
jorgehenriquezd <57832978+jorgehenriquezd@users.noreply.github.com>
2022-06-25 13:49:33 -0400

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,23 @@
 export const environment = {
-  production: true
+  production: true,
+  //===========Url Base==================
+  baseUrl: 'http://137.184.8.13/api',
+
+ //==============GET=====================
+  vehicle_types: '/vehicle-types',
+  vehicle_brands: '/vehicle-brands', 
+  banks: '/banks',
+  buildings: '/buildings',
+  categories: '/categories',
+  articles: '/articles',
+  places: '/places',
+
+  //============POST=====================  
+  carrier: '/applications/carrier',
+  promoter: '/applications/promoter',
+  freight: '/quotations/freights',
+  movings: '/quotations/movings',
+  calcule_volume: '/quotations/calculate-volume',
+
+  googleApiKey: 'AIzaSyA7lwfqRatEpjjXlIQOKHyrz0QtFMVp338'
 };
