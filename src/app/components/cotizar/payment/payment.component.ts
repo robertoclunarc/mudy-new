54d028980f0193a494de3b2d55a5fe83eed935f6
robertoclunarc <robertoclunarg@gmail.com>
2022-08-03 14:39:00 -0400

    inscripcion de pago

--- src/app/components/cotizar/payment/payment.component.ts
+++ src/app/components/cotizar/payment/payment.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { PostRequestService } from 'src/app/services/post-request.service';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Oneclick,Options, IntegrationApiKeys, Environment, IntegrationCommerceCodes  } from 'transbank-sdk';
 
 @Component({
   selector: 'app-payment',
@@ -12,26 +13,17 @@ export class PaymentComponent implements OnInit {
   
   stripePayment= {return_url: 'https://mudy.netlify.app/quotation/payment'};
   dataPayment:{ url?: any, token_ws?:any } = {};
-  formPayment!: FormGroup;
+  
+  @ViewChild('startForm') startForm!: ElementRef;
   
   constructor(
     private postService: PostRequestService,
     private http: HttpClient,
-    ) { this.buildForm(); this.payment() }  
+    ) { this.payment() }  
 
   ngOnInit(): void {    
-  }
+  }  
   
-  private buildForm() {
-    this.formPayment = new FormGroup({
-      token_ws: new FormControl('', [Validators.required]),      
-    });
-
-    this.formPayment.valueChanges
-    .subscribe(value => {
-      console.log(value);
-    });
-  }
 
   async payment() {
     
@@ -45,21 +37,9 @@ export class PaymentComponent implements OnInit {
     })
   }
   
-  onSubmit(event: Event){
+  async onSubmit(event: Event){
+    console.log(`url: ${this.dataPayment.url} / token:${this.dataPayment.token_ws}`)
+    this.startForm.nativeElement.submit();
     
-    this.formPayment.controls['token_ws'].setValue(this.dataPayment.token_ws)
-    event.preventDefault();
-    const value = this.formPayment.value;
-    console.log(`url: ${this.dataPayment.url}`);
-    console.log(value);
-    this.http.post(this.dataPayment.url, {token_ws: value.token_ws} )
-    .subscribe((result)=>{
-      console.warn(`result: ${result}`);
-      
-    }, ((error: HttpErrorResponse ) => {
-      console.log(error.error)       
-      
-    }));
-    window.open(this.dataPayment.url, "_blank");
   }  
 }
